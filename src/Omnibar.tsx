dc3dd6192b554c9777d9d2f4adf152a3113cf27d
Hamid Zare <hamidzare.vu@gmail.com>
2021-04-15 12:31:14 -0700

    h: improve result highlighting

--- src/Omnibar.tsx
+++ src/Omnibar.tsx
@@ -44,7 +44,7 @@ export default class Omnibar<T> extends React.PureComponent<
               ? results.slice(0, this.props.maxResults)
               : results,
           displayResults: results.length > 0,
-          selectedIndex: Math.min(this.state.selectedIndex, results.length - 1),
+          selectedIndex: results.length == 0 ? -1 : 0,
         });
         this.props.onQuery && this.props.onQuery(results);
       });
